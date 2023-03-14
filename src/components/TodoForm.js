3c13eee7bea780c85181fb62766684ebc1f008aa
Abhishek R <37078073+abhishek14k@users.noreply.github.com>
2023-03-14 23:41:32 +0530

    Added gh pages integration, TodoList basic heading

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 
-function TodoForm() {
+function TodoForm(props) {
   const [input, setInput] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input,
+    // });
+
+    setInput("");
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
   };
 
   return (
@@ -14,6 +26,7 @@ function TodoForm() {
         value={input}
         name="text"
         className="todo-input"
+        onChange={handleChange}
       />
       <button className="todo-button">Add todo</button>
     </form>
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import TodoForm from "./TodoForm";
+
+function TodoList() {
+  return (
+    <div>
+      <h1>What's the Plan for Today?</h1>
+      <TodoForm />
+    </div>
+  );
+}
+
+export default TodoList;
